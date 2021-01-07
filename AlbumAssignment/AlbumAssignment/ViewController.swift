4f0c7dd1fad7ddb9c7fb10bc368158135118561d
MahmoudFarid <alaakhalil093@gmail.com>
2021-01-07 16:22:04 +0100

    Initiate the project

---
+++ AlbumAssignment/AlbumAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumAssignment
+//
+//  Created by Alaa Khalil on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
